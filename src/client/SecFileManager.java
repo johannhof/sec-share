e93cd0fbd3a261685e13b3da6cf4396ffc6c4a07
João Caetano <joao.caet7@gmail.com>
2014-03-25 16:47:35 +0000

    added file sync comparing

--- src/client/SecFileManager.java
+++ src/client/SecFileManager.java
@@ -4,17 +4,22 @@ import file_services.FileInfo;
 import file_services.FileOperations;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 //This class implements the core file operations so they aren't all stacked up on main
 public class SecFileManager {
 
-    List<File> userFiles;
-    ServerStub myServer;
+	List<File> userFiles;
+	ServerStub myServer;
 	File clientHome;
 
-    public SecFileManager(List<File> clientFiles, ServerStub server, String clientHome) {
-        this.userFiles = clientFiles;
+	public SecFileManager(List<File> clientFiles, ServerStub server, String clientHome) {
+		this.userFiles = clientFiles;
 		this.myServer = server;
 		this.clientHome = new File(clientHome);
 	}
@@ -29,11 +34,17 @@ public class SecFileManager {
 		System.out.println(" +++ Files copied from server +++ ");
 	}
 
+	//TODO careful with empty file list will return false must add check to see if class is a list or a reply on the stub then return accordingly
 	public void listFiles() {
 		List<FileInfo> result = myServer.listFiles();
 		System.out.println(" +++ File list: +++ ");
+		
 		for(FileInfo fi : result)
 			System.out.println(result.toString() + "\n\n");		
+	}
+	
+	private List<FileInfo> getServerFileList(){
+		return myServer.listFiles();
 	}
 
 	//TODO CAREFUL with array / list size and order
@@ -50,18 +61,89 @@ public class SecFileManager {
 		}
 		System.out.println(" +++ File sharing complete +++ ");		
 	}
-	
+
+
 	//TODO
 	public void SyncFiles(int syncTimer) {
 		//TODO PROBLEM: WHEN getting a file, it has to replace old file if exists or create a new one then add to client list if it is a new share received
-				boolean running = true;
+		boolean running = true;
+		String input;
+		
+		System.out.println(" +++ File Synchronization Starting +++ ");		
+		System.out.println("");
+		System.out.println("Available commands: ");
+		System.out.println("\"exit\" - to stop and exit the program");
+		//main sync cycle
+		while (running) {
 
-		        while (running) {
+			System.out.println(" + New sync cycle + ");
+			//TODO split the uploads / downloads timewise			
+			
+			//get server file list
+			
+			Map<String, String> fileSyncStatus = fileListCompare();
+		}
 
-		        }
+		//every 30 synctimer seconds list compare upload download
+		//TODO SYNC, must open some user input stream to read commands to stop
+	}
 
-		        //every 30 synctimer seconds list compare upload download
-		        //TODO SYNC, must open some user input stream to read commands to stop
+	//TODO might need to add checks for lists not being empty
+	private Map<String, String> FileListCompare(List<FileInfo> serverFiles) {
+
+		//sets to contain file data
+		HashMap<String, Long> localFileData = new HashMap<String, Long>();
+		HashMap<String, Long> remoteFileData = new HashMap<String, Long>();
+
+		//unique filenames from local and server
+		Set<String> distinctFilenames = new HashSet<String>();
+
+		//results to return map of filename plus operation Map<filename, operation>
+		Map<String, String> result = new HashMap<String, String>();
+
+
+		//add local data to map and set
+		for(File f : userFiles){
+			localFileData.put(f.getName(), f.lastModified());	
+			distinctFilenames.add(f.getName());
+		}
+
+		//add remote data to map and set
+		for(FileInfo fi : serverFiles){
+			localFileData.put(fi.getFilename(), fi.getLastModified());
+			distinctFilenames.add(fi.getFilename());
+		}
+
+		//main method cycle 
+		for(String name : distinctFilenames){
+
+			//if the file isn't on the local list
+			if(!localFileData.containsKey(name)){
+				result.put(name, "get");
 			}
+			//if the file isn't on the remote file list
+			else if(!remoteFileData.containsKey(name)){
+				result.put(name, "put");
+			}
+			//general case where the file is on both
+			else {
+				long localTime = localFileData.get(name);
+				long remoteTime = localFileData.get(name);
+
+				//file is more recent on the client
+				if(localTime > remoteTime)
+					result.put(name,"put");
+
+				//file is more recent on the server
+				else if(remoteTime > localTime)
+					result.put(name,"get");
+
+				//both have the same timestamp
+				//do nothing
+			}
+		}
+
+		return result;
+	}
 
 }
